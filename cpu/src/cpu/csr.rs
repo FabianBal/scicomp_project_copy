ab8a991c42afeb203f61a2372b920d0d2bbd7bb7
Alireza Selahi <you@example.com>
2025-01-28 03:04:21 +0100

    CPU: CSR sparse mit CSR Ergebnis, erster Entwurf

--- cpu/src/cpu/csr.rs
+++ cpu/src/cpu/csr.rs
@@ -73,6 +73,12 @@ impl CSR {
 
 
 
+    pub fn get_row_nnz(&self, k: usize) -> usize {
+        self.row_pos[k+1]-self.row_pos[k]
+    }
+
+
+
     // Matrix/Matrix product, see seciton 3.2 from "A Systematic Survey of General Sparse Matrix-Matrix Multiplication", Gao et al.
     // https://doi.org/10.1145/3571157
     // Notation also from Paper
@@ -123,6 +129,118 @@ impl CSR {
         }
 
         mat
+    }
+
+
+
+
+    pub fn product_sparse(&self, other: &CSR) -> CSR {
+        // let data = vec![];
+        let m = self.shape.0;
+        let n = other.shape.1;
+        // let mut mat = vec![vec![0.;n];m];
+        let mut mat = Dense::new_zeros((m,n));
+
+        let mut res_rows: Vec<Vec<f64>> = vec![];
+        let mut res_col_idxs: Vec<Vec<usize>> = vec![];
+
+        for i in 0..m {
+            // iterate over all non-zero cols of A_{i*}
+            // let mut nnz = 0;            
+            // let mut row_indices_other = vec![];
+            // for col_pos_pos in other.row_pos[i]..other.row_pos[i+1] {
+            //     nnz += other.get_row_nnz(other.col_pos[col_pos_pos]);
+            //     // println!("AAA {} {}", k, col_pos_pos);
+            //     row_indices_other.push(other.col_pos[col_pos_pos]);
+            // }
+
+            
+            let mut nz_row_marker = vec![false;n];
+            let mut res_curr_row = vec![0.;n];
+
+
+
+            for col_pos_pos in self.row_pos[i]..self.row_pos[i+1] {
+                let k = self.col_pos[col_pos_pos];
+
+                for other_col_pos_pos in other.row_pos[k]..other.row_pos[k+1] {
+                    let j = other.col_pos[other_col_pos_pos];
+
+                    // C_{i*} = \sum_{k \in I_i (A)} a_{ik} b_{i*}
+                    // a_{ik} = self.values[col_pos_pos]
+                    // b_{kj} = other.values[other_col_pos_pos]
+                    // mat[i][j] += self.values[col_pos_pos] * other.values[other_col_pos_pos]
+                    // mat.set(i,j , mat.get(i, j) +   self.values[col_pos_pos] * other.values[other_col_pos_pos]);
+                    res_curr_row[j] += self.values[col_pos_pos] * other.values[other_col_pos_pos];
+                    nz_row_marker[j] = true;                    
+
+                }
+            }
+
+
+            // println!("in i={}, row fin {:?}", i, res_curr_row);
+            // println!("in i={}, nz idx {:?}", i, nz_row_marker);
+
+            // println!("LENS i={}: {} {} {}", i, (row_indices_other).len(), nz_row_marker.len(), res_curr_row.len());
+
+            let mut res_curr_row_final_val = vec![];
+            let mut res_curr_row_final_col_idx = vec![];
+            // for ((col_pos_pos, marker), x) in row_indices_other.iter().zip(nz_row_marker).zip(res_curr_row) {
+            for ((k, x), marker) in res_curr_row.iter().enumerate().into_iter().zip(nz_row_marker) {
+                // println!("aaaaa {} {} {}", other.col_pos[*col_pos_pos], marker, x);
+                if marker {
+                    res_curr_row_final_val.push(*x);
+                    res_curr_row_final_col_idx.push(k);
+                    // println!("AsaaAa {}", x);
+                }
+            }
+
+
+
+            res_rows.push(res_curr_row_final_val);
+            res_col_idxs.push(res_curr_row_final_col_idx);
+        }
+
+        for r in &res_rows {
+            println!("res rows {:?}", *r);
+        } 
+        for r in &res_col_idxs {
+            println!("res col idx {:?}", *r);
+        }
+
+
+
+
+        let mut row_pos_counter = 0;
+        let mut row_pos = vec![0];
+
+        for c in &res_col_idxs {
+            row_pos_counter += c.len();
+            row_pos.push(row_pos_counter);
+        }
+
+        let col_pos = res_col_idxs.concat();
+        let values = res_rows.concat();
+
+        row_pos.push(values.len());
+
+        CSR{row_pos, col_pos, values, shape: (m,n)}
+
+
+
+        // let mut col_pos = vec![];
+        // let mut values = vec![];
+
+        
+        // for (cols, vals) in res_col_idxs.iter().zip(res_rows) {
+        //     row_pos_counter += cols.len();
+        //     row_pos.push(row_pos_counter);
+
+        //     col_pos.concat()
+        // }
+        
+
+        // mat
     }
 
 
--- cpu/src/main.rs
+++ cpu/src/main.rs
@@ -16,9 +16,18 @@ fn main() {
     let res = csr.product(&csr);
     res.print();
 
+    let res2 = csr.product_sparse(&csr);
+    let res2d = res2.to_dense();
+    res2.print();
+    res2d.print();
 
-    let csr_d = csr.to_dense();
-    csr_d.print();
+    for k in 0..csr.shape.0 {
+        println!("nnz({}) = {}", k, csr.get_row_nnz(k));
+    }
+
+
+    // let csr_d = csr.to_dense();
+    // csr_d.print();
 
     // for row in res {
     //     for x in row {
@@ -31,11 +40,11 @@ fn main() {
     // d.print();
 
     // let a002 = COO::read_mtx("matrix_instances/generated/case_0000_A.mtx").expect(":(");
-    let mut a002 = COO::read_mtx(Path::new("../matrix_instances/generated/case_0000_A.mtx"), true).expect(":(");
+    // let mut a002 = COO::read_mtx(Path::new("../matrix_instances/generated/case_0000_A.mtx"), true).expect(":(");
     // a002.to_dense().print();
 
     // a002.data.sort_by(|a, b| (a.0, a.1).cmp(&(b.0, b.1)));
-    println!("{:?}", a002.data);
+    // println!("{:?}", a002.data);
 
 
 }
