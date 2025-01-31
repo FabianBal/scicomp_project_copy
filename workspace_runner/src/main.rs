976cbd025b6d7795a39ebd0feb70f47aeacc0d6e
Julian Sampels <julian.sampels@campus.tu-berlin.de>
2025-01-31 23:06:11 +0100

    Add repeat count parameter to benchmark_matrix function for improved benchmarking accuracy

--- workspace_runner/src/main.rs
+++ workspace_runner/src/main.rs
@@ -1,17 +1,20 @@
-use std::{env, fs, path::{Path, PathBuf}};
+use std::{env, fs, path::{Path, PathBuf}, vec};
 use matrix_base::{COO, CSR, Dense};
 
 /// Benchmark matrix multiplication using different libraries
 /// load all matrices from provided folder path or default and benchmark all possible combinations
 fn main() {
-    // Default folder path
+    // Default values
+    let mut repeat_count: usize = 10;
     let mut folder_path = "./matrix_instances";
 
-    // Get the folder path from command-line arguments if provided
+    // Get command-line arguments if provided
     let args: Vec<String> = env::args().collect();
     if args.len() >= 2 {
+        repeat_count = args[1].parse().expect("Failed to parse repeat count");
+    }
+    if args.len() >= 3 {
         folder_path = &args[1];
-
     }
 
     // search matrices in the folder
@@ -23,7 +26,7 @@ fn main() {
         for matrix2_path in &matrix_paths {
             //ToDo: need to make sure that the matrices are compatible
             println!("\nBenchmarking {} X {}", matrix1_path.file_name().unwrap().to_str().unwrap(), matrix2_path.file_name().unwrap().to_str().unwrap());
-            benchmark_matrix(matrix1_path, matrix2_path);
+            benchmark_matrix(matrix1_path, matrix2_path, repeat_count);
         }
     }
 }
@@ -36,19 +39,27 @@ fn import_matrix(matrix_path: &Path) -> (Dense, CSR) {
 }
 
 // Benchmark matrix multiplication
-fn benchmark_matrix(matrix1_path: &Path, matrix2_path: &Path) {
+fn benchmark_matrix(matrix1_path: &Path, matrix2_path: &Path, repeat_count: usize) {
     let (matrix1_dense, matrix1_csr) = import_matrix(matrix1_path);
     let (matrix2_dense, matrix2_csr) = import_matrix(matrix2_path);
 
-    let start = std::time::Instant::now();
-    cublas::multiply(&matrix1_dense, &matrix2_dense);
-    let cublas_time = start.elapsed().as_nanos();
-
-    let start = std::time::Instant::now();
-    cusparse::multiply(&matrix1_csr, &matrix2_csr);
-    let cusparse_time = start.elapsed().as_nanos();
-
-    println!("cuBLAS: {:.6} ns \t| cuSPARSE: {:.6} ns", cublas_time, cusparse_time);
+    let mut times_cublas = Vec::with_capacity(repeat_count);
+    let mut times_cusparse = Vec::with_capacity(repeat_count);
+    for _ in 1..=repeat_count {
+        let start = std::time::Instant::now();
+        cublas::multiply(&matrix1_dense, &matrix2_dense);
+        times_cublas.push(start.elapsed().as_nanos());
+        
+        let start = std::time::Instant::now();
+        cusparse::multiply(&matrix1_csr, &matrix2_csr);
+        times_cusparse.push(start.elapsed().as_nanos());
+        
+    }
+    
+    // Calculate average time
+    let times = vec![&times_cublas, &times_cusparse];
+    let avg_time: Vec<f64> = times.into_iter().map(|time| time.iter().sum::<u128>() as f64 / repeat_count as f64).collect();
+    println!("cuBLAS: {:.6} ns \t| cuSPARSE: {:.6} ns", avg_time[0], avg_time[1]);
 }
 
 // Returns all paths to matrices inside folder_path
