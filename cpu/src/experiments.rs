94531c67b0a0476b3e98c9cd9a16c713810adb55
Alireza Selahi <you@example.com>
2025-02-04 17:30:20 +0100

    CPU: updated testcase to work after refactor

--- cpu/src/experiments.rs
+++ cpu/src/experiments.rs
@@ -20,31 +20,31 @@ fn main() {
     for k in 0..n {
         println!("Loading k={}", k);
         let fname = Path::new(DATA_PATH).join(&Path::new(&format!("generated/large_{:04}_A.mtx", k)));
-        let A = COO::read_mtx(&fname, true).expect("Failed reading matrix during test");
+        let a = COO::read_mtx(&fname, true).expect("Failed reading matrix during test");
         let fname = Path::new(DATA_PATH).join(&Path::new(&format!("generated/large_{:04}_B.mtx", k)));
-        let B = COO::read_mtx(&fname, true).expect("Failed reading matrix during test");
+        let b = COO::read_mtx(&fname, true).expect("Failed reading matrix during test");
         // let fname = Path::new(DATA_PATH).join(&Path::new(&format!("generated/case_{:04}_C.mtx", k)));
         // let C = COO::read_mtx(&fname, true).expect("Failed reading matrix during test");
 
-        let A = CSR::from_coo(A);
-        let B = CSR::from_coo(B);
+        let a = CSR::from_coo(&a);
+        let b = CSR::from_coo(&b);
         // let C = C.to_dense();
         
         // sparse-to-dense
         let start_time = Instant::now();
-        let C = A.product(&B);
+        let c = a.product(&b);
         let duration= start_time.elapsed();
         times_s2d.push(duration.as_micros());
 
         // sparse-to-sparse
         let start_time = Instant::now();
-        let C = A.product_sparse(&B);
+        let c = a.product_sparse(&b);
         let duration= start_time.elapsed();
         times_s2s.push(duration.as_micros());
 
         // sparse-to-sparse parallel
         let start_time = Instant::now();
-        let C = A.product_sparse_par(&B);
+        let c = a.product_sparse_par(&b);
         let duration= start_time.elapsed();
         times_s2s_par.push(duration.as_micros());
 
