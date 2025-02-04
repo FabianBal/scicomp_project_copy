1f1f5d6773286002c12fcd07de698bb093a50900
Julian Sampels <julian.sampels@campus.tu-berlin.de>
2025-02-04 21:37:21 +0100

    adjusted paths

--- generate_matrices_experiments.py
+++ generate_matrices_experiments.py
@@ -16,15 +16,19 @@ import argparse
 pfad = "matrix_instances/generated"
 
 
+### Anzahl der Exemplare
+number_of_examples = 3
+
 ### DENSE
 # Größe variieren
-sizes_dense = [10, 100] # Dimensionen
+sizes_dense = [x for x in range(10,501, 10)] # Dimensionen
 
 
 ## SPARSE
 # Größe variieren
-sparsity = 0.15 # Fixe Sparsity
-sizes_sparse = [30, 100, 1000, 10000, 100000, 300000] # Dimensionen
+sparsity = 2 # Fixe Sparsity
+sizes_sparse = list(range(10, 501, 10)) # Dimensionen
+sizes_sparse.sort()
 
 
 ## DENSE vs SPARSE
@@ -53,79 +57,80 @@ def write_mtx_dense(fname, B):
                 f.write("%d %d %.20f\n" %(i+1,j+1, B[i,j]))
 #############################
 
-
-### DENSE
-for n in sizes_dense:
-    # Generate
-    A = np.random.rand(n, n)
-    B = np.random.rand(n, n)
-    
-    
-    # Write
-    Path(join(pfad, "dense")).mkdir(parents=True, exist_ok=True)
+for example_count in range(0, number_of_examples):
+    example_count = str(example_count).zfill(len(str(number_of_examples)))
+    ### DENSE
+    for n in sizes_dense:
+        # Generate
+        A = np.random.rand(n, n)
+        B = np.random.rand(n, n)
         
-    fname = join(pfad, "dense", "dense_" + str(n) + "_A.mtx")
-    write_mtx_dense(fname, A)
-    fname = join(pfad, "dense", "dense_" + str(n) + "_B.mtx")
-    write_mtx_dense(fname, B)
-    
-
-### Sparse
-for n in sizes_sparse:     
-    # Paths
-    Path(join(pfad, "sparse")).mkdir(parents=True, exist_ok=True)
-    fname_A = join(pfad, "sparse", "sparse_" + str(n) + "_A.mtx")
-    fname_B = join(pfad, "sparse", "sparse_" + str(n) + "_B.mtx")
-    
-    with open(fname_A, "w") as f:
-        N = int(n*sparsity) #  Anzahl Einträge gesamt
-        f.write("%d %d %d\n" % (n, n, N))
         
-        for _ in range(0, N):
-            i = np.random.randint(1, n)
-            j = np.random.randint(1, n)
-            x = np.random.rand()
-            f.write("%d %d %.20f\n" %(i+1,j+1,x))
+        # Write
+        Path(join(pfad, "dense")).mkdir(parents=True, exist_ok=True)
             
-    with open(fname_B, "w") as f:
-        N = int(n*sparsity) #  Anzahl Einträge gesamt
-        f.write("%d %d %d\n" % (n, n, N))
+        fname = join(pfad, "dense", "dense_" + str(n) + "_A" + example_count + ".mtx")
+        write_mtx_dense(fname, A)
+        fname = join(pfad, "dense", "dense_" + str(n) + "_B" + example_count + ".mtx")
+        write_mtx_dense(fname, B)
         
-        for _ in range(0, N):
-            i = np.random.randint(1, n)
-            j = np.random.randint(1, n)
-            x = np.random.rand()
-            f.write("%d %d %.20f\n" %(i+1,j+1,x))
-    
-    
-### Sparse vs Dense
-for spa in sparsities:
-    # Paths
-    Path(join(pfad, "sparse_vs_dense")).mkdir(parents=True, exist_ok=True)
-    fname_A = join(pfad, "sparse_vs_dense", "sparse_vs_dense_" + str(spa) + "_A.mtx")
-    fname_B = join(pfad, "sparse_vs_dense", "sparse_vs_dense_" + str(spa) + "_B.mtx")
-    
-    n = size_sparse_vs_dense
-    
-    with open(fname_A, "w") as f:
-        N = int(size_sparse_vs_dense*spa) #  Anzahl Einträge gesamt
-        f.write("%d %d %d\n" % (n, n, N))
+
+    ### Sparse
+    for n in sizes_sparse:     
+        # Paths
+        Path(join(pfad, "sparse")).mkdir(parents=True, exist_ok=True)
+        fname_A = join(pfad, "sparse", "sparse_" + str(n) + "_A" + example_count + ".mtx")
+        fname_B = join(pfad, "sparse", "sparse_" + str(n) + "_B" + example_count + ".mtx")
         
-        for _ in range(0, N):
-            i = np.random.randint(1, n)
-            j = np.random.randint(1, n)
-            x = np.random.rand()
-            f.write("%d %d %.20f\n" %(i+1,j+1,x))
+        with open(fname_A, "w") as f:
+            N = int(n*sparsity) #  Anzahl Einträge gesamt
+            f.write("%d %d %d\n" % (n, n, N))
             
-    with open(fname_B, "w") as f:
-        N = int(size_sparse_vs_dense*spa) #  Anzahl Einträge gesamt
-        f.write("%d %d %d\n" % (n, n, N))
+            for _ in range(0, N):
+                i = np.random.randint(1, n)
+                j = np.random.randint(1, n)
+                x = np.random.rand()
+                f.write("%d %d %.20f\n" %(i+1,j+1,x))
+                
+        with open(fname_B, "w") as f:
+            N = int(n*sparsity) #  Anzahl Einträge gesamt
+            f.write("%d %d %d\n" % (n, n, N))
+            
+            for _ in range(0, N):
+                i = np.random.randint(1, n)
+                j = np.random.randint(1, n)
+                x = np.random.rand()
+                f.write("%d %d %.20f\n" %(i+1,j+1,x))
         
-        for _ in range(0, N):
-            i = np.random.randint(1, n)
-            j = np.random.randint(1, n)
-            x = np.random.rand()
-            f.write("%d %d %.20f\n" %(i+1,j+1,x))
+        
+    ### Sparse vs Dense
+    for spa in sparsities:
+        # Paths
+        Path(join(pfad, "sparse-vs-dense")).mkdir(parents=True, exist_ok=True)
+        fname_A = join(pfad, "sparse-vs-dense", "sparse-vs-dense_" + str(spa) + "_A" + example_count + ".mtx")
+        fname_B = join(pfad, "sparse-vs-dense", "sparse-vs-dense_" + str(spa) + "_B" + example_count + ".mtx")
+        
+        n = size_sparse_vs_dense
+        
+        with open(fname_A, "w") as f:
+            N = int(size_sparse_vs_dense*spa) #  Anzahl Einträge gesamt
+            f.write("%d %d %d\n" % (n, n, N))
+            
+            for _ in range(0, N):
+                i = np.random.randint(1, n)
+                j = np.random.randint(1, n)
+                x = np.random.rand()
+                f.write("%d %d %.20f\n" %(i+1,j+1,x))
+                
+        with open(fname_B, "w") as f:
+            N = int(size_sparse_vs_dense*spa) #  Anzahl Einträge gesamt
+            f.write("%d %d %d\n" % (n, n, N))
+            
+            for _ in range(0, N):
+                i = np.random.randint(1, n)
+                j = np.random.randint(1, n)
+                x = np.random.rand()
+                f.write("%d %d %.20f\n" %(i+1,j+1,x))
     
 
     
